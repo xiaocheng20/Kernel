FedShat/msm-4.4

drivers: touchscreen: Import platina drivers
* From nitrogen-q-oss

include/linux/input/synaptics_tcm.h
@@ -35,6 +35,9 @@
 
 #define I2C_MODULE_NAME "synaptics_tcm_i2c"
 #define SPI_MODULE_NAME "synaptics_tcm_spi"
+#ifdef CONFIG_MACH_XIAOMI_PLATINA
+#define WAKEUP_GESTURE
+#endif
 
 struct syna_tcm_board_data {
 	bool x_flip;
@@ -45,6 +48,9 @@ struct syna_tcm_board_data {
 	int power_gpio;
 	int power_on_state;
 	int reset_gpio;
+	#ifdef CONFIG_MACH_XIAOMI_PLATINA
+	int mdss_reset;
+	#endif
 	int reset_on_state;
 	unsigned int spi_mode;
 	unsigned int power_delay_ms;
@@ -58,6 +64,11 @@ struct syna_tcm_board_data {
 	unsigned long irq_flags;
 	const char *pwr_reg_name;
 	const char *bus_reg_name;
+	#ifdef CONFIG_MACH_XIAOMI_PLATINA
+	const char *i2c_reg_name;
+	const char *lab_reg_name;
+	const char *ibb_reg_name;
+	#endif
 };
 
 #endif

xiaocheng20/Kernel

Change-Id: Ib7e86526d81daebdb731922205e1d3e430319c94

include/linux/input/synaptics_tcm.h
@@ -35,6 +35,9 @@
 
 #define I2C_MODULE_NAME "synaptics_tcm_i2c"
 #define SPI_MODULE_NAME "synaptics_tcm_spi"
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_TCM_MI
+#define WAKEUP_GESTURE
+#endif
 
 struct syna_tcm_board_data {
 	bool x_flip;
@@ -45,6 +48,9 @@ struct syna_tcm_board_data {
 	int power_gpio;
 	int power_on_state;
 	int reset_gpio;
+	#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_TCM_MI
+	int mdss_reset;
+	#endif
 	int reset_on_state;
 	unsigned int spi_mode;
 	unsigned int power_delay_ms;
@@ -58,6 +64,11 @@ struct syna_tcm_board_data {
 	unsigned long irq_flags;
 	const char *pwr_reg_name;
 	const char *bus_reg_name;
+	#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_TCM_MI
+	const char *i2c_reg_name;
+	const char *lab_reg_name;
+	const char *ibb_reg_name;
+	#endif
 };
 
 #endif